33557649cdc1d257539834fc49071058a33eac0b
xmyl-153 <2068437087@qq.com>
2026-08-27 14:02:19 +0800

    Auto sync source code for Vercel 🚀

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ import ClickEffect from "../components/ClickEffect";
 import BackgroundSlider from "../components/BackgroundSlider";
 import GlobalToolbox from "../components/GlobalToolbox";
 import SplashScreen from "../components/SplashScreen";
-import CyberCat from '../components/CyberCat';
 import DanmakuBackground from '../components/DanmakuBackground';
 
 import MobileBackButton from '../components/MobileBackButton';
@@ -127,10 +126,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
               }
             `}} />
           </MusicProvider>
-
-          <div className="hidden md:block">
-            <CyberCat />
-          </div>
 
         </ThemeProvider>
       </body>
